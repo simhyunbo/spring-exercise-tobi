be7b7436d276ca2749039957de6821278175f230
simhyunbo <hbo9347@naver.com>
2022-10-25 12:54:30 +0900

    deleteAll getcount test 추가

--- src/test/java/likelion/UserDaoTest.java
+++ src/test/java/likelion/UserDaoTest.java
@@ -27,4 +27,23 @@ class UserDaoTest {
         User user = userDao.findById(id);
         Assertions.assertEquals("kim",user.getName());
     }
+
+    @Test
+    void deleteAndCount() throws SQLException, ClassNotFoundException {
+        UserDao userDao = context.getBean("awsUserDao",UserDao.class);
+        userDao.deleteAll();
+        User user1 = new User("1","sim","1234");
+        User user2 = new User("2","kim","5678");
+        User user3 = new User("3","park","qwer");
+
+        userDao.add(user1);
+        assertEquals(1,userDao.getCount());
+        userDao.add(user2);
+        assertEquals(2,userDao.getCount());
+        userDao.add(user3);
+        assertEquals(3,userDao.getCount());
+    }
+
+
+
 }
